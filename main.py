8d7708b5c0d71306805493c58472621be6189599
Alex <aleo999@gmail.com>
2019-02-25 13:46:48 +0100

    first

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy
+import sklearn
+import scipy
+import matplotlib
+
+
+#Cxd array
+mu ={1,1}
+#Cxdxd array
+sigma ={1,1,1}
+
+#X: set of row vector, labels: class labels for each data points
+def mlParams(X, labels):
+    return 0
+
